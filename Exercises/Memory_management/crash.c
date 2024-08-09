69b9d1bbf460e5582ba868e2212cdda8117de0d5
neenu-sagi <95954006+neenu-sagi@users.noreply.github.com>
2024-08-09 23:48:24 +0100

    Update crash.c

--- Exercises/Memory_management/crash.c
+++ Exercises/Memory_management/crash.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-        //read-only mm
+        //read-only mm accessing it results in crash
 	        char *pStr = "this is stored in the text segment";
 		        pStr[0] = 'n';
 			        return 0;
